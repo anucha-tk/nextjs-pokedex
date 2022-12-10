4bcd9d8b2bae98a4d48fcf8b09428be8ed934b66
Anucha.tk <anuchataowkaen@gmail.com>
2022-12-10 10:33:05 +0700

    feat: separate pokemon homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,7 @@
-import Image from 'next/image';
 import * as React from 'react';
 
 import Layout from '@/components/layout/Layout';
-import Paginate from '@/components/paginate/paginate';
-
-import { useGetPokemonNamesQuery } from '@/store/slices/pokemonSlice';
+import Pokemons from '@/components/Pokemons';
 
 /**
  * SVGR Support
@@ -18,85 +15,10 @@ import { useGetPokemonNamesQuery } from '@/store/slices/pokemonSlice';
 // Before you begin editing, follow all comments with `STARTERCONF`,
 // to customize the default configuration.
 
-const emoji: { [key: string]: string } = {
-  grass: '🍃',
-  poison: '🧪',
-  hp: '❤️',
-  attack: '⚔',
-  defense: '🛡️',
-  'special-attack': '💥',
-  'special-defense': '🔰',
-  speed: '🚀',
-  height: '📏',
-  weight: '⚖️',
-  types: '🔡',
-};
-
 export default function HomePage() {
-  const [page, setPage] = React.useState<number>(0);
-  const { data: pokemons } = useGetPokemonNamesQuery(page);
-
   return (
     <Layout>
-      <div className='mx-8 mb-6 grid gap-4 lg:grid-cols-3'>
-        {pokemons?.map((pokemon) => {
-          const types = pokemon.types.map((e) => e.type.name);
-          const states = pokemon.stats.map((e) => e);
-          return (
-            <div
-              key={pokemon.id}
-              className='rounded-lg border p-4 shadow-md hover:bg-gray-100'
-            >
-              <h5 className='text-2xl font-bold uppercase tracking-tight text-gray-900'>
-                {pokemon.name}
-              </h5>
-
-              <div className='flex flex-col items-center justify-center sm:flex-row'>
-                <Image
-                  src={pokemon.sprites.front_default}
-                  alt='pokemon'
-                  width={150}
-                  height={150}
-                />
-                <div className='p-4'>
-                  {states.map(({ stat, base_stat }) => (
-                    <h5
-                      key={stat.name}
-                      className='mb-2 font-bold text-gray-700'
-                    >
-                      <span className='mr-3'>{emoji[stat.name]}</span>
-                      <span>
-                        {stat.name.toUpperCase()}: {base_stat}
-                      </span>
-                    </h5>
-                  ))}
-                  <h5 className='mb-2 font-bold text-gray-700'>
-                    <span className='mr-3'>{emoji['height']}</span>
-                    Height: {pokemon.height}
-                  </h5>
-                  <h5 className='mb-2 font-bold text-gray-700'>
-                    <span className='mr-3'>{emoji['weight']}</span>
-                    weight: {pokemon.weight}
-                  </h5>
-                  <h5 className='mb-2 font-bold text-gray-700'>
-                    <span className='mr-3'>{emoji['types']}</span>
-                    Types:{' '}
-                    {types.map((name) => (
-                      <span key={name}>
-                        {emoji[name]} {name}
-                      </span>
-                    ))}
-                  </h5>
-                  <p></p>
-                </div>
-              </div>
-            </div>
-          );
-        })}
-      </div>
-      <div className='flex justify-center'>
-        <Paginate page={page} setPage={setPage} />
-      </div>
+      <Pokemons />
     </Layout>
   );
 }
